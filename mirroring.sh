cd29ff32b436ca10af5546c29da25ed39414480e
Brice Friha <contact@bricefriha.dev>
2024-10-12 21:07:20 +0100

    use ffplay to watch streaming

--- mirroring.sh
+++ mirroring.sh
@@ -7,3 +7,6 @@ ffmpeg -f avfoundation -i "1:0" -r 30 -s 1280x720 -f mpegts udp://localhost:1234
 
 # Connect to the simulator stream
 ssh -L 12345:localhost:12345 $MAC_username@$MAC_ip
+
+# play the broadcast 
+ffplay udp://@:12345
